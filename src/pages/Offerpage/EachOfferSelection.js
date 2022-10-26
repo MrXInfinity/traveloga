b8bcea69d838110b248aa2cb9c1658f6de66f508
Johann Mendoza <johannmendoza1209@gmail.com>
2022-10-26 14:43:41 +0800

    changed css in offerpage

--- src/pages/Offerpage/EachOfferSelection.js
+++ src/pages/Offerpage/EachOfferSelection.js
@@ -18,21 +18,20 @@ const EachOfferSelection = ({image, title, location, description, limitedOffers,
         <img className='h-2/5 md:h-full w-full md:w-1/2 xl:w-[110%] xl:max-w-[620px] object-cover object-center' src={image} alt={title}/>
         <div className="flex flex-col h-3/5 md:h-full lg:justify-between lg:py-4 xl:py-6 lg:pl-8">
             <div className="flex flex-col h-3/4 md:h-3/4 lg:h-fit p-4 lg:p-0 text-black/[0.9] justify-around lg:justify-start">
-                <div className="flex items-end mb-2 lg:mb-4 xl:mb-6 ">
-                    <h1 className='mr-2 text-xl xl:text-2xl'>{`${title}, `}</h1>
-                    <h2 className=' '>{location}</h2>
+                <div className="flex items-end mb-1 md:mb-4 ">
+                    <h1 className='mr-2 text-lg md:text-xl lg:text-2xl'>{title}, </h1>
+                    <h2 className='text-sm md:text-base'>{location}</h2>
                 </div>
-                <p className='text-left mb-2 lg:mb-4 xl:mb-6'>{description}</p>
+                <p className='text-left mb-2 lg:mb-4 xl:mb-6 text-sm md:text-base'>{description}</p>
                 {Object.keys(limitedOffers).map((eachOffer, array)=>(
-                    <div className="flex lg:last:mt-2" key={array}>
+                    <div className="flex lg:last:mt-2 items-center" key={array}>
                         <FontAwesomeIcon className="text-2xl text-[#2B8E9B] mr-2" icon={faTag} />
-                        <h1 className='capitalize mr-2'>{`${eachOffer}: `}</h1>
-                        <h2 className='text-red-700'>{`${limitedOffers[eachOffer]}% OFF `}</h2>
+                        <h1 className='capitalize mr-2 text-sm md:text-base'>{eachOffer}: </h1>
+                        <h2 className='text-red-700 md:text-lg'>{limitedOffers[eachOffer]}% OFF </h2>
                     </div>
                 ))}
             </div>
             <div className="flex w-full md:w-fit h-fit md:h-fit items-center p-4 md:py-2 xl:px-6 xl:py-3 bg-amber-200 hover:bg-amber-300 hover:text-white justify-center transition duration-300 ease-in-out" onClick={()=>{bookingToggleUI(_id)}} >
-                <FontAwesomeIcon className="text-2xl lg:text-xl mr-2 lg:mr-4 " icon={faCartPlus} />
                 <p className=''>ADD TO CART</p>
             </div>
         </div>
--- src/pages/Offerpage/OfferHeader.js
+++ src/pages/Offerpage/OfferHeader.js
@@ -3,10 +3,10 @@ import React from 'react'
 const OfferHeader = () => { 
   
   return (
-  <header className='flex h-60 md:h-64 lg:h-72 w-10/12 bg-cover mt-40 mb-10 md:mb-12 mx-auto bg-center bg-[url("https://images.pexels.com/photos/8031711/pexels-photo-8031711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")]'>
-    <div className="flex flex-col justify-center items-center max-w-[215px] md:w-[37%] lg:w-[29%] lg:max-w-[240px] xl:w-[20%] py-4 px-2 m-auto bg-amber-300 font-['Spinnaker'] rounded-full text-center text-[#004852] " >
-      <h1 className="font-['Rubik'] text-3xl md:text-4xl">SUMMER PROMOS!</h1>
-      <h2 className="md:text-lg">Avail our limited offers only available during the summer</h2>
+  <header className='flex h-48 md:h-64 lg:h-72 w-10/12 bg-cover mt-36 md:mt-40 mb-10 md:mb-12 mx-auto bg-center bg-[url("https://images.pexels.com/photos/8031711/pexels-photo-8031711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")]'>
+    <div className="flex flex-col justify-center items-center max-w-[175px] md:w-[37%] lg:w-[29%] lg:max-w-[240px] xl:w-[20%] py-4 px-2 m-auto bg-amber-300 font-['Spinnaker'] rounded-full text-center text-[#004852] " >
+      <h1 className="font-['Rubik'] text-2xl md:text-3xl lg:text-4xl">SUMMER PROMOS!</h1>
+      <h2 className="text-sm md:text-base lg:text-lg">Avail our limited offers only available during the summer</h2>
       <h3 className="font-semibold text-sm md:text-base">6/21 - 9/23</h3>
     </div>
   </header>
