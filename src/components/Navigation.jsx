6d0bd20e046bc1f142a553cc02d63d80515c8c08
Johann Mendoza <johannmendoza1209@gmail.com>
2023-05-13 21:41:27 +0800

    changed js to jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,144 @@
+import { faBars, faCompass, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Menu, Transition } from '@headlessui/react';
+import React, { Fragment, useRef } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import { useGlobalContext } from '../context.js';
+import Footer from './Footer/Footer.js';
+import TransitionComponent from './TransitionWrapper.js';
+import useLocalStorage from '../hooks/useLocalStorage.js';
+
+const Nav = () => {
+  const { user } = useGlobalContext();
+  const { localStorageValues } = useLocalStorage('authenticated');
+  const navRef = useRef(null);
+
+  const navItems = [
+    ['/', 'Home'],
+    ['/offers', 'Offers'],
+    ['/destinations', 'Destinations'],
+    ['/about-us', 'About Us'],
+  ];
+
+  const scrollUp = () => {
+    navRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <div className="flex flex-col" ref={navRef}>
+      <div className="flex justify-center">
+        <div className="fixed top-0 z-10 flex w-full max-w-[100rem] items-center justify-between bg-black/40 px-5 pt-5 pb-6 lg:px-12 lg:py-6">
+          <div className="flex items-center gap-2 ">
+            <FontAwesomeIcon
+              className="mt-2 text-3xl text-white md:mt-0 "
+              icon={faCompass}
+            />
+            <div className="flex flex-col ">
+              <h1 className="font-Rubik text-lg text-white lg:text-xl">
+                TRAVELOGA
+              </h1>
+              <h1 className="whitespace-nowrap text-xs text-white ">
+                Experiencing Philippines
+              </h1>
+            </div>
+          </div>
+
+          {/* Mobile Navigation */}
+          <div className="flex">
+            <Menu as="div" className=" sm:hidden">
+              {({ open }) => (
+                <>
+                  <div>
+                    <Menu.Button className="flex h-14 w-14 items-center justify-center">
+                      <FontAwesomeIcon
+                        className="text-3xl text-amber-300 transition-all duration-300 ease-in-out"
+                        icon={open ? faXmark : faBars}
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-150"
+                    enterFrom="transform opacity-0 -translate-y-1"
+                    enterTo="transform opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150 "
+                    leaveFrom="transform opacity-100 translate-y-0"
+                    leaveTo="transform opacity-0 -translate-y-1">
+                    <Menu.Items className="absolute right-0 mt-6 flex items-center">
+                      <div className="  flex flex-col items-center justify-around bg-white text-lg ">
+                        {navItems.map(([link, title], index) => (
+                          <Menu.Item onClick={() => scrollUp()} key={index}>
+                            <NavLink
+                              to={link}
+                              className="w-full border-transparent py-4 px-6 md:text-white"
+                              style={({ isActive }) => {
+                                return { color: isActive ? '#fbbf24' : '' };
+                              }}>
+                              {title}
+                            </NavLink>
+                          </Menu.Item>
+                        ))}
+                        <Menu.Item onClick={() => scrollUp()}>
+                          <NavLink
+                            to={
+                              localStorageValues && user
+                                ? '/personal-account'
+                                : '/login'
+                            }
+                            className=" py-4 px-6 hover:text-amber-400"
+                            style={({ isActive }) => {
+                              return { color: isActive ? '#fbbf24' : '' };
+                            }}>
+                            {user && localStorageValues
+                              ? `${user.firstname}'s Bookings`
+                              : `Login / Signup`}
+                          </NavLink>
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </>
+              )}
+            </Menu>
+
+            {/* Non Mobile Nav */}
+            <div className="right-0 hidden h-fit flex-row items-center justify-between gap-3 sm:flex md:gap-4">
+              {navItems.map(([link, title], index) => (
+                <NavLink
+                  to={link}
+                  key={index}
+                  className="button_transition w-fit text-center text-white transition-all hover:border-b-4 hover:border-amber-300"
+                  style={({ isActive }) => {
+                    return {
+                      color: isActive ? '#fde68a' : '',
+                      borderBottom: isActive ? `4px solid #fde68a` : ``,
+                    };
+                  }}
+                  onClick={() => scrollUp()}>
+                  {title}
+                </NavLink>
+              ))}
+              <NavLink
+                to={localStorageValues && user ? '/personal-account' : '/login'}
+                className=" button_transition rounded-3xl  bg-amber-300 py-2 px-4 text-center text-sm hover:bg-amber-300 hover:text-white lg:px-4 lg:py-3 "
+                style={({ isActive }) => {
+                  return {
+                    backgroundColor: isActive && '#fcd34d',
+                    color: isActive && 'white',
+                  };
+                }}
+                onClick={() => scrollUp()}>
+                {user && localStorageValues ? `My Bookings` : `Login / Signup`}
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+      <Footer />
+      <TransitionComponent />
+    </div>
+  );
+};
+
+export default Nav;
