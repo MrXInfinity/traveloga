466a383b37b676c8db82aa367e31b4c520091416
Johann Mendoza <johannmendoza1209@gmail.com>
2023-05-15 19:26:11 +0800

    minor adjustment to code order

--- src/pages/Homepage/EachHomeSelection.jsx
+++ src/pages/Homepage/EachHomeSelection.jsx
@@ -13,37 +13,7 @@ const Selection = ({ info, leftPosition }) => {
     setTransitionOpen(true);
   };
 
-  if (info.length > 0) {
-    return (
-      <>
-        <div className="flex overflow-auto overscroll-none">
-          <div
-            className="relative flex w-fit gap-10 pb-2 transition-all delay-150 duration-500 ease-in-out lg:snap-none lg:px-2"
-            style={{ left: `${-leftPosition * 35}rem` }}>
-            {info.map(({ title, image, location, _id }) => (
-              <div
-                className="button_transition flex w-[22rem] flex-col shadow-lg transition-all last:mr-6 hover:shadow-md hover:shadow-black sm:w-[400px] md:w-[522px] md:last:mr-0 "
-                key={_id}
-                onClick={() => destinationToggleUI(_id)}>
-                <img
-                  src={image}
-                  alt={title}
-                  className="aspect-video h-full object-cover object-center  "
-                />
-                <div className="flex items-center justify-between py-3 px-4">
-                  <h1 className=" md:text-lg lg:text-xl ">{title}</h1>
-                  <div className="flex items-center gap-2 text-sm text-black/70 md:text-base lg:text-black/70">
-                    <FontAwesomeIcon icon={faLocationDot} />
-                    <h1>{location}</h1>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </>
-    );
-  } else {
+  if (info.length < 1) {
     return (
       <div className="flex h-80 w-[22rem] animate-pulse flex-col overflow-hidden shadow-lg sm:w-[400px] md:w-[522px] ">
         <div className=" aspect-video h-[90%] bg-slate-200" />
@@ -57,6 +27,36 @@ const Selection = ({ info, leftPosition }) => {
       </div>
     );
   }
+
+  return (
+    <>
+      <div className="flex overflow-auto overscroll-none">
+        <div
+          className="relative flex w-fit gap-10 pb-2 transition-all delay-150 duration-500 ease-in-out lg:snap-none lg:px-2"
+          style={{ left: `${-leftPosition * 35}rem` }}>
+          {info.map(({ title, image, location, _id }) => (
+            <div
+              className="button_transition flex w-[22rem] flex-col shadow-lg transition-all last:mr-6 hover:shadow-md hover:shadow-black sm:w-[400px] md:w-[522px] md:last:mr-0 "
+              key={_id}
+              onClick={() => destinationToggleUI(_id)}>
+              <img
+                src={image}
+                alt={title}
+                className="aspect-video h-full object-cover object-center  "
+              />
+              <div className="flex items-center justify-between py-3 px-4">
+                <h1 className=" md:text-lg lg:text-xl ">{title}</h1>
+                <div className="flex items-center gap-2 text-sm text-black/70 md:text-base lg:text-black/70">
+                  <FontAwesomeIcon icon={faLocationDot} />
+                  <h1>{location}</h1>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Selection;
